8d76d38471d7947923eb44b23c43446bbb0e9c3e
Redy1aye <86603844+Redy1aye@users.noreply.github.com>
2021-07-17 16:08:32 +0500

    Add files via upload

---
+++ src/main/java/dev/redy1aye/copperequipment/Mod.java
@@ -0,0 +1,33 @@
+package dev.redy1aye.copperequipment;
+
+import dev.redy1aye.copperequipment.armor.Armor;
+import dev.redy1aye.copperequipment.armor.BaseArmor;
+import dev.redy1aye.copperequipment.tools.*;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class Mod implements ModInitializer {
+
+    public static final String MOD_ID = "copperequipment";
+
+    public static final ArmorMaterial COPPER_ARMOR = new Armor();
+
+    @Override
+    public void onInitialize() {
+        Registry.register(Registry.ITEM, new Identifier(Mod.MOD_ID, "copper_shovel"), new Shovel(new Tools()));
+        Registry.register(Registry.ITEM, new Identifier(Mod.MOD_ID, "copper_pickaxe"), new Pickaxe(new Tools()));
+        Registry.register(Registry.ITEM, new Identifier(Mod.MOD_ID, "copper_axe"), new Axe(new Tools()));
+        Registry.register(Registry.ITEM, new Identifier(Mod.MOD_ID, "copper_hoe"), new Hoe(new Tools()));
+
+        Registry.register(Registry.ITEM, new Identifier(Mod.MOD_ID, "copper_sword"), new Sword(new Tools()));
+
+        Registry.register(Registry.ITEM, new Identifier(Mod.MOD_ID, "copper_helmet"), new BaseArmor(COPPER_ARMOR, EquipmentSlot.HEAD));
+        Registry.register(Registry.ITEM, new Identifier(Mod.MOD_ID, "copper_chestplate"), new BaseArmor(COPPER_ARMOR, EquipmentSlot.CHEST));
+        Registry.register(Registry.ITEM, new Identifier(Mod.MOD_ID, "copper_leggings"), new BaseArmor(COPPER_ARMOR, EquipmentSlot.LEGS));
+        Registry.register(Registry.ITEM, new Identifier(Mod.MOD_ID, "copper_boots"), new BaseArmor(COPPER_ARMOR, EquipmentSlot.FEET));
+
+    }
+}
