c0d70bb44df582b33339e2eeb0bd7ccbed9fe0fa
Redy1aye <86603844+Redy1aye@users.noreply.github.com>
2021-09-01 22:48:32 +0500

    Add files via upload

---
+++ src/main/java/dev/redy1aye/copperequipment/CopperEquipmentConfig.java
@@ -0,0 +1,57 @@
+package dev.redy1aye.copperequipment;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.serializer.PartitioningSerializer;
+import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
+
+@Config(name = "Copper Equipment Configs")
+public class CopperEquipmentConfig extends PartitioningSerializer.GlobalData {
+
+    public General CE_CFG = new General();
+    public General.WGeneral WCE_CFG = new General.WGeneral();
+
+    @Config(name = "Copper Equipment")
+        public static class General implements ConfigData {
+
+            @Comment("Copper Equipment Mod Config!"
+                + "\n")
+        public int CopperArmorDurability = 100; @Comment("+ 12 \uD83D\uDC52; 98 \uD83D\uDC55; 79 \uD83D\uDC56; 34 \uD83E\uDD7E.") // + 12 👒; 98 👕; 79 👖; 34 🥾. Default = 100.
+        public int CopperToolsDurability = 176;
+            @Comment("// //")
+        public int CopperHelmetProtection = 3; // Default = 3.
+        public int CopperChestplateProtection = 6; // Default = 6.
+        public int CopperLeggingsProtection = 5; // Default = 5.
+        public int CopperBootsProtection = 2; // Default = 2.
+            @Comment("// //")
+        public int CopperSwordDamage = 6; // Default = 6.
+        public float CopperSwordAttackSpeed = -2.3f; // Default = -2.3.
+
+        public int CopperAxeDamage = 9; // Default = 9.
+        public float CopperAxeAttackSpeed = -3.1f; // Default = -3.1.
+            @Comment("// //")
+        public int CopperToolsMiningLevel = 2; // Default = 2.
+
+        @Config(name = "Waxed Copper Equipment")
+        public static class WGeneral implements ConfigData {
+
+                @Comment("Copper Equipment Mod! Waxed Config!"
+                    + "\n")
+            public int WaxedCopperArmorDurability = 200; @Comment("+ 12 \uD83D\uDC52; 98 \uD83D\uDC55; 79 \uD83D\uDC56; 34 \uD83E\uDD7E.") // + 12 👒; 98 👕; 79 👖; 34 🥾. Default = 200.
+            public int WaxedCopperToolsDurability = 276; // Default = 276.
+                @Comment("// //")
+            public int WaxedCopperHelmetProtection = 2; // Default = 2.
+            public int WaxedCopperChestplateProtection = 5; // Default = 5.
+            public int WaxedCopperLeggingsProtection = 4; // Default = 4.
+            public int WaxedCopperBootsProtection = 1; // Default = 1.
+                @Comment("// //")
+            public int WaxedCopperSwordDamage = 6; // Default = 6.
+            public float WaxedCopperSwordAttackSpeed = -2.5f; // Default = -2.5.
+
+            public int WaxedCopperAxeDamage = 9; // Default = 9.
+            public float WaxedCopperAxeAttackSpeed = -3.3f; // Default = -3.3.
+                @Comment("// //")
+            public int WaxedCopperToolsMiningLevel = 2; // Default = 2.
+        }
+    }
+}
---
+++ src/main/java/dev/redy1aye/copperequipment/armor/WaxedCopperArmor.java
@@ -0,0 +1,65 @@
+package dev.redy1aye.copperequipment.armor;
+
+import dev.redy1aye.copperequipment.Mod;
+
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Items;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+
+public class WaxedCopperArmor implements ArmorMaterial {
+
+    static int WaxedCopperArmorDurability = Mod.CONFIG.WCE_CFG.WaxedCopperArmorDurability;
+
+    static int WaxedCopperHelmetProtection = Mod.CONFIG.WCE_CFG.WaxedCopperHelmetProtection;
+    static int WaxedCopperChestplateDProtection = Mod.CONFIG.WCE_CFG.WaxedCopperChestplateProtection;
+    static int WaxedCopperLeggingsProtection = Mod.CONFIG.WCE_CFG.WaxedCopperLeggingsProtection;
+    static int WaxedCopperBootsProtection = Mod.CONFIG.WCE_CFG.WaxedCopperBootsProtection;
+
+    public static final ArmorMaterial WAXED_COPPER_ARMOR = new WaxedCopperArmor();
+
+    private static final int[] DURABILITY = new int[] {34, 79, 98, 12};
+    private static final int[] PROTECTION = new int[] {WaxedCopperBootsProtection, WaxedCopperLeggingsProtection, WaxedCopperChestplateDProtection, WaxedCopperHelmetProtection};
+
+    @Override
+    public int getDurability(EquipmentSlot slot) {
+        return DURABILITY[slot.getEntitySlotId()] + WaxedCopperArmorDurability;
+    }
+
+    @Override
+    public int getProtectionAmount(EquipmentSlot slot) {
+        return PROTECTION[slot.getEntitySlotId()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 13;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return SoundEvents.ITEM_ARMOR_EQUIP_GOLD;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(Items.COPPER_INGOT);
+    }
+
+    @Override
+    public String getName() {
+        return "waxed_copper";
+    }
+
+    @Override
+    public float getToughness() {
+        return 0;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return 0;
+    }
+}
