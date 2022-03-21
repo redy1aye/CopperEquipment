0e331a4c50f1e31fe09ac2a04764e4c39196df39
Redy1aye <86603844+Redy1aye@users.noreply.github.com>
2022-03-21 10:59:32 +0500

    Delete ModShears.java

--- src/main/java/dev/redy1aye/copperequipment/tools/ModShears.java
+++
@@ -1,9 +0,0 @@
-package dev.redy1aye.copperequipment.tools;
-
-import net.minecraft.item.ShearsItem;
-
-public class ModShears extends ShearsItem {
-    public ModShears(Settings settings) {
-        super(settings);
-    }
-}
